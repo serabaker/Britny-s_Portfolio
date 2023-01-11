c6bd9e63c3d57267933504dc9aee9bdf72099032
BritnyLain <bejaylain@gmail.com>
2023-01-10 18:05:16 -0600

    added divider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./components/Home/Home";
+import Divider from "./components/Divider/Divider";
 
 function App() {
   return (
     <div>
       <Home />
+      <Divider />
     </div>
   );
 }
