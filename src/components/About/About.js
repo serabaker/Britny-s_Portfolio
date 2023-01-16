d2e687d9cb0d59f5a58b3bd1d8468870d9655e42
BritnyLain <bejaylain@gmail.com>
2023-01-15 18:23:41 -0600

    added about files

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div>
+      <p>Wamp wamp what it do, what it do. Im Britny</p>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import "./Home.css";
 import Projects from "../Projects/Projects";
 import Navbar from "../Navbar/Navbar";
 import Blog from "../Blog/Blog";
+import About from "../About/About";
 
 const Home = () => {
   const portfolioName = ["Britny Lain"];
@@ -11,6 +12,7 @@ const Home = () => {
       <div id="about">
         <>{<Navbar />}</>
         <h1 className="home-h1">{portfolioName}</h1>
+        <About />
         <>{<Projects />}</>
         <>{<Blog />}</>
       </div>
