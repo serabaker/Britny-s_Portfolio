7af0fa1c252c730675697e9d79a1eeafc4a02c77
BritnyLain <bejaylain@gmail.com>
2023-01-20 20:22:41 -0600

    added project cards and truncated project detailson server

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,46 +13,138 @@ const Projects = () => {
 
   return (
     <div id="project" className="project-container">
-      <p className="project-home">Projects</p>
-      {/* <div className="display-container"> */}
-      <h3 className="home-h3">
-        <a
-          className="a-connet4Vets"
-          href="https://d3urubd6wwveok.cloudfront.net/"
-        >
-          Connect 4 Vets
-        </a>
-      </h3>
-      <div className="home-2">
+      <div className="cards">
+        <div className="project-img">
+          <img className="img-1" src={connet4Vets} alt="connect 4 vets app" />
+        </div>
+        <div className="project-title">
+          <a
+            className="a-connet4Vets"
+            href="https://d3urubd6wwveok.cloudfront.net/"
+          >
+            Connect 4 Vets
+          </a>
+        </div>
+        <div className="project-text">
+          {typeof backendData.projects === "undefined" ? (
+            <p>Loading...</p>
+          ) : (
+            backendData.projects.map((projects, key) => (
+              <p className="para-2" key={key}>
+                {projects}
+              </p>
+            ))
+          )}
+        </div>
+        <div className="tech-list">
+          <ul className="project-ul-list">
+            <li className="project-li-list">
+              Front End created with: Mui | React.js(with Vite) | Tailwind CSS |
+              Visual Studio Code
+            </li>
+            <li className="project-li-list">
+              Backend created with: AWS Amplify Hosting | AWS S3 | Graphql
+            </li>
+            <li className="project-li-list"> Version Control: Github</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="cards">
+        <div className="project-img">
+          <img className="img-1" src={connet4Vets} alt="connect 4 vets app" />
+        </div>
+        <div className="project-title">
+          <a
+            className="a-connet4Vets"
+            href="https://d3urubd6wwveok.cloudfront.net/"
+          >
+            Connect 4 Vets
+          </a>
+        </div>
+        <div className="project-text">
+          {typeof backendData.projects === "undefined" ? (
+            <p>Loading...</p>
+          ) : (
+            backendData.projects.map((projects, key) => (
+              <p className="para-2" key={key}>
+                {projects}
+              </p>
+            ))
+          )}
+        </div>
+        <div className="tech-list">
+          <ul className="project-ul-list">
+            <li className="project-li-list">
+              Front End created with: Mui | React.js(with Vite) | Tailwind CSS |
+              Visual Studio Code
+            </li>
+            <li className="project-li-list">
+              Backend created with: AWS Amplify Hosting | AWS S3 | Graphql
+            </li>
+            <li className="project-li-list"> Version Control: Github</li>
+          </ul>
+        </div>
+      </div>
+      <div className="cards">
+        <div className="project-img">
+          <img className="img-1" src={connet4Vets} alt="connect 4 vets app" />
+        </div>
+        <div className="project-title">
+          <a
+            className="a-connet4Vets"
+            href="https://d3urubd6wwveok.cloudfront.net/"
+          >
+            Connect 4 Vets
+          </a>
+        </div>
+        <div className="project-text">
+          {typeof backendData.projects === "undefined" ? (
+            <p>Loading...</p>
+          ) : (
+            backendData.projects.map((projects, key) => (
+              <p className="para-2" key={key}>
+                {projects}
+              </p>
+            ))
+          )}
+        </div>
+        <div className="tech-list">
+          <ul className="project-ul-list">
+            <li className="project-li-list">
+              Front End created with: Mui | React.js(with Vite) | Tailwind CSS |
+              Visual Studio Code
+            </li>
+            <li className="project-li-list">
+              Backend created with: AWS Amplify Hosting | AWS S3 | Graphql
+            </li>
+            <li className="project-li-list"> Version Control: Github</li>
+          </ul>
+        </div>
+      </div>
+      {/* <p className="project-home">Projects</p>
+      <div className="display-container">
+        <title className="home-h3">
+          <a
+            className="a-connet4Vets"
+            href="https://d3urubd6wwveok.cloudfront.net/"
+          >
+            Connect 4 Vets
+          </a>
+        </title>
+       
         <a
           className="a-screenshot"
           href="https://d3urubd6wwveok.cloudfront.net/"
         >
-          <img className="img-1" src={connet4Vets} alt="connect 4 vets app" />{" "}
+          {" "}
         </a>
-        {typeof backendData.projects === "undefined" ? (
-          <p>Loading...</p>
-        ) : (
-          backendData.projects.map((projects, key) => (
-            <p className="para-2" key={key}>
-              {projects}
-            </p>
-          ))
-        )}
-      </div>
-      <div className="list-of-tech">
-        <ul className="project-ul-list">
-          <li className="project-li-list">
-            Front End created with: Mui | React.js(with Vite) | Tailwind CSS |
-            Visual Studio Code
-          </li>
-          <li className="project-li-list">
-            Backend created with: AWS Amplify Hosting | AWS S3 | Graphql
-          </li>
-          <li className="project-li-list"> Version Control: Github</li>
-        </ul>
-      </div>
-      {/* </div> */}
+       
+
+        <div className="list-of-tech">
+          
+        </div>
+      </div> */}
     </div>
   );
 };
