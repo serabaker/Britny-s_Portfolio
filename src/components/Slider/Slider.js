4480c8756571d8006813d12336e579691e1cc4b6
BritnyLain <bejaylain@gmail.com>
2023-01-31 07:12:56 -0600

    added new projects

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Slider.css";
 import projectImg from "../../images/Connect4Vets.png";
-
+import weatherproject2 from "../../images/weatherproject2.png";
+import opalsorganic from "../../images/opalsorganic.png";
 const Slider = () => {
   return (
     <div className="slider">
@@ -21,24 +22,30 @@ const Slider = () => {
       <div className="slide">
         <img
           className="slide-image"
-          src={projectImg}
+          src={weatherproject2}
           alt="connect 4 vets application"
         />
         <div className="app-title">
-          <a className="slider-a" href="https://d3urubd6wwveok.cloudfront.net/">
-            <span>Connect 4 Vets</span>
+          <a
+            className="slider-a"
+            href="https://master.d2qmd27n0cuzo.amplifyapp.com/"
+          >
+            <span>daily.weather</span>
           </a>
         </div>
       </div>
       <div className="slide">
         <img
           className="slide-image"
-          src={projectImg}
+          src={opalsorganic}
           alt="connect 4 vets application"
         />
         <div className="app-title">
-          <a className="slider-a" href="https://d3urubd6wwveok.cloudfront.net/">
-            <span>Connect 4 Vets</span>
+          <a
+            className="slider-a"
+            href="https://master.d1hypkpxf0f9rp.amplifyapp.com/ "
+          >
+            <span>opal's organic</span>
           </a>
         </div>
       </div>
