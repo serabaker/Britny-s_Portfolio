63f96171afef2550002ad55a6a3e63a2f1f1c2ae
BritnyLain <bejaylain@gmail.com>
2023-03-05 10:11:23 -0600

    updated about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,14 +5,15 @@ const About = () => {
   return (
     <div id="about" className="about">
       <h1 className="h1">About</h1>
-      <aside className="aside">
+      <div className="aside">
         <p className="p">
           Thanks for taking time to visit us. We are a dedicated company serving
           small and mid-size businesses who are looking to give their webites as
-          a facelift. This could includes updating outdated features, routing
-          and/or design{" "}
+          a facelift. This could includes updating outdated features, optimizing
+          page load times, adding animations and other UX design ideas you have
+          in mind.
         </p>
-      </aside>
+      </div>
     </div>
   );
 };
